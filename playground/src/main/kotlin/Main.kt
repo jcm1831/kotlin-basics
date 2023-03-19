9d3a36ab8dddd37491851e557bdd1fd65756b46c
Goesswein, Manuel <manuel.goesswein@tum.de>
2023-03-19 17:11:23 +0100

    Remove playground project.

--- playground/src/main/kotlin/Main.kt
+++ playground/src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun getNumbersCount(): Int {
+    val separator = File.separator
+    val fileName = "src${separator}words_with_numbers.txt"
+    var numbersCount = 0
+    File(fileName).forEachLine { line ->
+        if (line.toCharArray().all { char -> return@all char.isDigit() }) {
+            ++numbersCount
+        }
+    }
+    return numbersCount
+}
+
+fun findLongestWord(): String {
+    val separator = File.separator
+    val fileName = "src${separator}words_sequence.txt"
+    var longestWord = ""
+    File(fileName).forEachLine { line ->
+        if (line.length > longestWord.length) {
+            longestWord = line
+        }
+    }
+    return longestWord
+}
+
+fun main(args: Array<String>) {
+    println(getNumbersCount())
+    println(findLongestWord().length)
 }
